008bffd153b1ab27fd6c772b1ce1d299697b0808
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-12-27 16:08:08 +0200

    sp

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,6 +5,14 @@ export const Loader = () => {
   return (
     <LoaderStyled>
       <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
       />
     </LoaderStyled>
   );
