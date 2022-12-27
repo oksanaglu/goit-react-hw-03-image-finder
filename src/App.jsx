c9a7f8daf0baf7b60d669efa89d5eaf9d712a3c7
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-12-27 18:02:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { toast } from 'react-toastify';
-import SearchBar from 'components/SearchBar';
+import SearchBar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
 import Loader from 'components/Loader';
 import Button from 'components/Button';
